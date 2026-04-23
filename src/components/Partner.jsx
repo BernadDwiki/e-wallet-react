a7bec130f6fd77aa286a95cbe30f8578f2d58055
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-04-23 15:03:21 +0700

    feat: add progress

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,11 +1,11 @@
 const Partner = () => (
-  <section className="bg-[#e8e8e84d] flex flex-col md:flex-row w-full justify-center items-center px-6 md:px-[100px] py-[10px] gap-5">
-    <img src="./assets/Group 21.png" className="px-10 py-5 w-lg md:w-auto" />
-    <img src="./assets/Group 19.png" className="px-10 py-5 w-lg md:w-auto" />
-    <img src="./assets/Group 23.png" className="px-10 py-5 w-lg md:w-auto" />
-    <img src="./assets/logo 1.png"   className="px-10 py-5 w-lg md:w-auto" />
-    <img src="./assets/Group 17.png" className="px-10 py-5 w-lg md:w-auto" />
-    <img src="./assets/Group 18.png" className="px-10 py-5 w-lg md:w-auto" />
+  <section className="bg-[#e8e8e84d] flex flex-col md:flex-row w-full justify-between items-center px-6 md:px-[100px] py-[10px] gap-5">
+    <img src="./assets/Group 21.png" />
+    <img src="./assets/Group 19.png" />
+    <img src="./assets/Group 23.png" />
+    <img src="./assets/logo 1.png"   />
+    <img src="./assets/Group 17.png" />
+    <img src="./assets/Group 18.png" />
   </section>
 );
 
