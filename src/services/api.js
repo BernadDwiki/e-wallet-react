6c1acdc6ebf23b59a69c5f486783ca0c51268e53
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-05-31 08:01:16 +0700

    feat: add api integration

---
+++ src/services/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("has_pin");
+      localStorage.removeItem("isAuthenticated");
+      localStorage.removeItem("persist:root");
+      window.location.href = "/auth/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
